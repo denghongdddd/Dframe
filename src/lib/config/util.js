d562626cae8d600fc6e5c4538ea62c12768688dc
denghongdddd <m13724205240@163.com>
2020-12-21 11:18:25 +0800

    refresh

--- src/lib/config/util.js
+++ src/lib/config/util.js
@@ -2,3 +2,41 @@
 export const type=function(target,str){
 	return Object.prototype.toString.call(target).slice(8,-1).toLocaleLowerCase()===str
 }
+
+/**订阅者 */
+export function provise(){
+	let subscriber={}
+	this.on=(name,callback)=>{
+		if(name&&typeof callback === "function"){
+			let callbackList = subscriber[name]||[]
+			if((subscriber[name]||[]).indexOf(callback)<0){
+				callbackList.push(callback)
+				subscriber[name]=callbackList
+			}
+		}
+	}
+	
+	this.emit=(name,...arr)=>{
+		if(name&&subscriber[name]){
+			var callbackList=[].concat(subscriber[name])
+			callbackList.forEach(v=>{
+				if(subscriber[name].indexOf(v)>=0){
+					v(...arr)
+				}
+			})
+		}
+	}
+	
+	this.off=(name,callback)=>{
+		if(name&&subscriber[name]){
+			if(typeof callback === "function"){
+				var index=subscriber[name].indexOf(callback)
+				if(index>=0)subscriber[name].splice(index,1)
+				if(subscriber[name].length==0)
+				delete subscriber[name]
+			}else{
+				delete subscriber[name]
+			}
+		}
+	}
+}
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,13 @@
 import "./config/customeDate.js"
 import vuePayKeyboard from './components/vue-pay-keyboard' // 导入组件
+import {type} from "./config/util.js"
 
 const paykeyboard = {
     install(Vue, options) {
         Vue.component(vuePayKeyboard.name, vuePayKeyboard)
+        Vue.prototype.utils={
+            type
+        }
     }
 }
 if (typeof window !== 'undefined' && window.Vue) {
